2188621d1a975d8ce789ba19347777a84767377d
eyall11223@gmail.com <eyall11223@gmail.com>
2022-08-07 16:28:40 +0300

    inputValue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Vip from './data/vip';
-import './App.css';
+
 
 
 function App() {
--- src/data/vip.js
+++ src/data/vip.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 function Vip() {
+let changeSelect = useRef();
+let changePrice = useRef();
+let [matach,improve] = useState();
+
+const changeMany = () => {
+    let many = changeSelect.current.value * changePrice.current.value;
+   improve(many);
+}
+
+    
   return (
     <div className="col-lg-6 p-3 shadow mt-3 mx-auto" >
        <h2>Enter the coin you want:</h2>
-       <select className="form-select">
+       <select onChange={changeMany} className="form-select" ref={changeSelect}>
         <option value="3.3">UST</option>
         <option value="3.9">EURO</option>
         <option value="0.11">BATH</option>
        </select>
        <h2>Enter the coin you want:</h2>
-       <input type="number" className="form-control"/>
-       <h3 className="mt-4">you will get --- NIS</h3>
+       <input type="number" placeholder="1" className="form-control" ref={changePrice}/>
+       <h3 className="mt-4">you will get {matach} NIS</h3>
     </div>
   )
 }
