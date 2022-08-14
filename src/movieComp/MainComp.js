c1067936ae630125b47f36501245f47b969e17f1
eyall11223@gmail.com <eyall11223@gmail.com>
2022-08-14 14:46:05 +0300

    start project

--- src/movieComp/MainComp.js
+++ src/movieComp/MainComp.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export const MainComp = () => {
   return (
-    <div>
+    <div className='container d-flex-3'>
+   <img src='' ></img>     
+  <h2>שם התמונה</h2>
+  <p>סיפור קל על התמונה</p>
 
-        
     </div>
   )
 }
